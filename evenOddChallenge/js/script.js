accd91988118e013dadab8e8d88976c9bf94bce7
KingofLimbs-1 <milesyusuf123@gmail.com>
2023-01-29 23:26:29 +0200

    Additional file creation
    Changes made:
    
    1. Created JS + HTML + CSS folders & added relevant files to each folder.
    
    2. Linked CSS + JS files to HTML

---
+++ evenOddChallenge/js/script.js
@@ -0,0 +1,15 @@
+// User input 
+
+// var userInput = prompt("Please enter a number: ")
+
+// // Even check
+
+// if(userInput %2 == 0){
+//     alert("That number is even!")
+// }
+
+// // Odd check
+
+// else{
+//     alert("That number is odd!")
+// }

e3539021fb7b33c730023b82d59c7b50dc976153
KingofLimbs-1 <milesyusuf123@gmail.com>
2023-01-30 00:35:45 +0200

    Deleted files

---
+++ evenOddChallenge/script.js
@@ -0,0 +1,16 @@
+// User input 
+
+var userInput = prompt("Please enter a number: ")
+
+// Even check
+
+if(userInput %2 == 0){
+    alert("That number is even!")
+}
+
+// Odd check
+
+else{
+    alert("That number is odd!")
+}
+

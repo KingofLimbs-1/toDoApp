7cadb36f115752308333d86f772177ee2b4eac5b
KingofLimbs-1 <milesyusuf123@gmail.com>
2023-01-31 04:03:58 +0200

    Continued Working
    Changes Made:
    
    1. Created input fields + wrapped them in div's for styling later on. (HTML)
    
    2. Added onclick() event to HTML and linked it to calculation function in JS (HTML)
    
    3. Created variables + linked to HTML elements (JS)

---
+++ three-input-calculator/js/if-calculator.js
@@ -0,0 +1,8 @@
+function calculate() {
+
+    // Variables
+    var input1 = parseFloat(document.getElementById("input1").value); // First input field (num)
+    var input2 = document.getElementById("input2").value; // Second input field (operator)
+    var input3 = parseFloat(document.getElementById("input3").value); // Third input field (num)
+
+}

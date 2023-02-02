9ae090a648f8fbeee2ecaaba6cbd7c560c0dc026
KingofLimbs-1 <milesyusuf123@gmail.com>
2023-02-02 16:55:20 +0200

    Switch Calculator - Began Working
    Changes made:
    
    1. Created page header + Created input boxes + Created result element (HTML)
    
    2. Created JS variables and linked to  relevant HTML elements. (JS)

---
+++ three-input-calculator/js/switch-calculator.js
@@ -0,0 +1,9 @@
+// Variables
+function calculate() {
+
+    var input1 = parseFloat(document.getElementById("input1").value); // Input 1
+    var input2 = parseFloat(document.getElementById("input2").value); // Input 2
+    var input3 = document.getElementById("input3").value; // Input 3
+    var result;
+
+}

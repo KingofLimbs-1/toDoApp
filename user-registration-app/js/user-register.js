928ff8d5133ea58fb11de8abcede91e9e14f5689
Miles Pieterse <99418553+KingofLimbs-1@users.noreply.github.com>
2023-03-05 16:41:06 +0200

    Began Working
    Changes Made:
    
    1. Created references for "view user list" button, "register user" button, "form field inputs", and "user table list" - JS
    
    2. Displayed "register user" button by adding a "display: block" - CSS

--- user-registration-app/js/user-register.js
+++ user-registration-app/js/user-register.js
@@ -14,15 +14,28 @@ console.log(userTabs);
 
 /* --- Nav references --- */
 
-// code here...
+// References the "View User List" button
+const userView = document.getElementById("register-tab-button");
+console.log(userView);
+
+// References the "Register" button
+const userRegistration = document.getElementById("users-tab-button");
+console.log(userRegistration);
 
 /* --- Form field references --- */
 
-// code here...
+// References form input fields
+// Subject to change though. Not sure if I'm referencing the right part.
+// Instead of the input tag, maybe I should be referencing the form itself. 
+// Only referenced it like this cause I wanted referencing to be specific to input fields, not <h1> tag as well for example.
+// Will come back to this. 
+const formFields = document.getElementsByTagName("input");
+console.log(formFields);
 
 /* --- Table list reference --- */
 
-// code here...
+const tableList = document.getElementById("user-list");
+console.log(tableList);
 
 /* ----------------------------------------------------
     Functionality

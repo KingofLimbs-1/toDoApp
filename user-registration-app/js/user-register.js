cb0752192dc7f253bd23f7b2551a5f468e508b0e
Miles Pieterse <99418553+KingofLimbs-1@users.noreply.github.com>
2023-03-06 01:15:31 +0200

    Continued Working
    Changes Made:
    
    1. Altered form inputs referencing. - JS
    
    2. Created a second event listener that handles the "register user button" - JS
    
    3. Created a third event listener that handles the "submit button"
    
    4. Created the "create new user" function. - JS
    
    5. Created the "create user element function" - JS

--- user-registration-app/js/user-register.js
+++ user-registration-app/js/user-register.js
@@ -24,16 +24,11 @@ console.log(userView);
 
 /* --- Form field references --- */
 
-// References form input fields
-// Subject to change though. Not sure if I'm referencing the right part.
-// Instead of the input tag, maybe I should be referencing the form itself.
-// Only referenced it like this cause I wanted referencing to be specific to input fields, not <h1> tag as well for example.
-// Will come back to this.
-const formFields = document.getElementsByTagName("input");
+// Form reference
+const formFields = document.getElementById("register-form");
 console.log(formFields);
 
 /* --- Table list reference --- */
-
 const tableList = document.getElementById("user-list");
 console.log(tableList);
 
@@ -41,18 +36,98 @@ console.log(tableList);
     Functionality
 ---------------------------------------------------- */
 
+/* --- "Create New User" Function --- */
+
+// Input data retrieval
+const firstName = document.getElementById("firstname").value;
+const lastName = document.getElementById("lastname").value;
+const username = document.getElementById("username").value;
+const email = document.getElementById("email").value;
+const password = document.getElementById("password").value;
+
+// User object data storage
+const users = [];
+
+// Function
+function createUser(firstName, lastName, username, email, password) {
+  const newUser = {
+    firstName: firstName,
+    lastName: lastName,
+    username: username,
+    email: email,
+    password: password,
+  };
+
+  users.push(newUser);
+}
+
+/* --- "Create User Element" Function --- */
+
+// Function
+function createUserElement(userElement) {
+  // New DOM element creation
+  const userDisplay = document.createElement("li");
+  // Adding to the entry class
+  userDisplay.classList.add("entry");
+
+  // New span elements creation
+
+  // Username
+  const usernameSpan = document.createElement("span");
+  usernameSpan.textContent = userElement.username;
+
+  // First Name
+  const firstNameSpan = document.createElement("span");
+  firstNameSpan.textContent = userElement.firstName;
+
+  // Last Name
+  const lastNameSpan = document.createElement("span");
+  lastNameSpan.textContent = userElement.lastName;
+
+  // Email
+  const emailSpan = document.createElement("span");
+  emailSpan.textContent = userElement.email;
+
+  // Password
+  const passwordSpan = document.createElement("span");
+  passwordSpan.textContent = userElement.password;
+
+  // Adding newly created span elements to userDisplay element
+
+  userDisplay.append(usernameSpan);
+  userDisplay.append(firstNameSpan);
+  userDisplay.append(lastNameSpan);
+  userDisplay.append(emailSpan);
+  userDisplay.append(passwordSpan);
+
+  // Function return
+
+  return userDisplay;
+}
+
 /* ----------------------------------------------------
     Event Listenters and Interactivity
 ---------------------------------------------------- */
 
-// View User List Button (On-click interactivity)
+// View User List Button (Event listener/On-click interactivity)
 // Added a kind of "toggle on/off" functionality to this event listener.
+userView.addEventListener("click", function () {
+  const viewUsers = document.querySelector("section.users");
+  if (viewUsers.style.display === "none" || viewUsers.style.display === "") {
+    viewUsers.style.display = "block";
+  } else {
+    viewUsers.style.display = "none";
+  }
+});
 
-userView.addEventListener("click", function() {
-    const viewUsers = document.querySelector("section.users");
-    if (viewUsers.style.display === "none" || viewUsers.style.display === "") {
-        viewUsers.style.display = "block";
-    } else {
-        viewUsers.style.display = "none";
-    }
+// Register User Button (Event listener/On-click interactivity)
+userRegister.addEventListener("click", function () {
+  const registerSection = document.querySelector("section.register");
+  registerSection.style.display = "block";
+});
+
+// Submit Button (Event listener)
+// Preventing form submission default behavior
+formFields.addEventListener("submit", function (event) {
+  event.preventDefault();
 });

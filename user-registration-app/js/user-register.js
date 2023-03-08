eba3ac41eaa5684c0377ba806c24fa0f7a4abd58
Miles Pieterse <99418553+KingofLimbs-1@users.noreply.github.com>
2023-03-08 17:26:29 +0200

    Continued Working
    Changes Made:
    
    1. Changed "userDisplay" class name to "newUsers" .Just made more sense. Updated class references as well.
    
    2. Created the "populateUserList" function.
    
    3. Updated my "submit" button event listener with user creation and user list population logic.

--- user-registration-app/js/user-register.js
+++ user-registration-app/js/user-register.js
@@ -66,9 +66,9 @@ function createUser(firstName, lastName, username, email, password) {
 // Function
 function createUserElement(userElement) {
   // New DOM element creation
-  const userDisplay = document.createElement("li");
+  const newUsers = document.createElement("li");
   // Adding to the entry class
-  userDisplay.classList.add("entry");
+  newUsers.classList.add("entry");
 
   // New span elements creation
 
@@ -92,17 +92,33 @@ function createUserElement(userElement) {
   const passwordSpan = document.createElement("span");
   passwordSpan.textContent = userElement.password;
 
-  // Adding newly created span elements to userDisplay element
+  // Adding newly created span elements to "newUsers" element
 
-  userDisplay.append(usernameSpan);
-  userDisplay.append(firstNameSpan);
-  userDisplay.append(lastNameSpan);
-  userDisplay.append(emailSpan);
-  userDisplay.append(passwordSpan);
+  newUsers.append(usernameSpan);
+  newUsers.append(firstNameSpan);
+  newUsers.append(lastNameSpan);
+  newUsers.append(emailSpan);
+  newUsers.append(passwordSpan);
 
   // Function return
 
-  return userDisplay;
+  return newUsers;
+}
+
+/* --- "Populate User List" Function --- */
+
+// Function
+function populateUserList(users) {
+  for (i = 0; i < users.length; i++) {
+    // User List HTML retrieval
+    const userList = document.getElementById("user-list");
+    // "user" array element initialization
+    const user = users[i];
+    // "createUserElement" function call
+    const li = createUserElement(user);
+    // Appending "li" to "userList"
+    userList.append(li);
+  }
 }
 
 /* ----------------------------------------------------
@@ -110,7 +126,7 @@ function createUserElement(userElement) {
 ---------------------------------------------------- */
 
 // View User List Button (Event listener/On-click interactivity)
-// Added a kind of "toggle on/off" functionality to this event listener.
+// Added a "toggle on/off" functionality to this event listener.
 userView.addEventListener("click", function () {
   const viewUsers = document.querySelector("section.users");
   if (viewUsers.style.display === "none" || viewUsers.style.display === "") {
@@ -127,7 +143,29 @@ userRegister.addEventListener("click", function () {
 });
 
 // Submit Button (Event listener)
-// Preventing form submission default behavior
+
+// Submit button Test
+
 formFields.addEventListener("submit", function (event) {
+  // Prevent form default behavior
   event.preventDefault();
+
+  // Grabbing input elements
+  const firstName = document.getElementById("firstname").value;
+  const lastName = document.getElementById("lastname").value;
+  const username = document.getElementById("username").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  // "createUser" function call. Takes input elements as parameters.
+  createUser(firstName, lastName, username, email, password);
+  // "populateUserList" function call. Adding created user to user list.
+  populateUserList(users);
+
+  //   Clear input fields
+  document.getElementById("firstname").value = "";
+  document.getElementById("lastname").value = "";
+  document.getElementById("username").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("password").value = "";
 });

54374816a55b2be236d6a2e21625c2e77c06fc56
Miles Pieterse <99418553+KingofLimbs-1@users.noreply.github.com>
2023-03-13 19:08:12 +0200

    Began Working
    Changes Made:
    
    1. Added HTML elements to page + Created user interactivity buttons (HTML)
    
    2. Created references to HTML elements I'll be needing (JS)
    
    3. Added styling to page (CSS)

---
+++ user-list-challenge/js/userList.js
@@ -0,0 +1,21 @@
+// HTML References
+
+// 3 Users button
+const addThreeUsers = document.getElementsByClassName("three-user-button");
+console.log(addThreeUsers);
+
+// Remove User Button
+const removeUsers = document.getElementsByClassName("remove-user-button");
+console.log(removeUsers);
+
+// Display User Button
+const displayUsers = document.getElementsByClassName("display-user-button");
+console.log(displayUsers);
+
+// Sort User Button
+const sortUsers = document.getElementsByClassName("sort-user-button");
+console.log(sortUsers);
+
+// User List 
+const userList = ["PixelPalooza","CosmicChaos","MindMeldMaster","SnackAttack87","NeonNinja","TechTornado","MysticMuffin","AquaAce","SkySurferX","SparklingSapphire"];
+

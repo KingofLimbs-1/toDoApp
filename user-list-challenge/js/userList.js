7308ef8d6b6b2cf965f4ed1e19ad71d5f3ddfa8f
Miles Pieterse <99418553+KingofLimbs-1@users.noreply.github.com>
2023-03-14 14:22:25 +0200

    Continued Working
    Changes Made:
    
    1. Changed button identifiers to ID's from classes. (HTML)
    
    2. Referenced button elements using querySelector instead of class names (JS)
    
    3. Organized code using comments + created event listener structures. Have not begun adding functionality yet. (JS)

--- user-list-challenge/js/userList.js
+++ user-list-challenge/js/userList.js
@@ -1,21 +1,66 @@
-// HTML References
+/* --- HTML Element References --- */ 
 
-// 3 Users button
-const addThreeUsers = document.getElementsByClassName("three-user-button");
+// Add 3 Users button
+const addThreeUsers = document.querySelector("#three-user-button");
 console.log(addThreeUsers);
 
 // Remove User Button
-const removeUsers = document.getElementsByClassName("remove-user-button");
+const removeUsers = document.querySelector("#remove-user-button");
 console.log(removeUsers);
 
 // Display User Button
-const displayUsers = document.getElementsByClassName("display-user-button");
+const displayUsers = document.querySelector("#display-user-button");
 console.log(displayUsers);
 
 // Sort User Button
-const sortUsers = document.getElementsByClassName("sort-user-button");
+const sortUsers = document.querySelector("#sort-user-button");
 console.log(sortUsers);
 
-// User List 
-const userList = ["PixelPalooza","CosmicChaos","MindMeldMaster","SnackAttack87","NeonNinja","TechTornado","MysticMuffin","AquaAce","SkySurferX","SparklingSapphire"];
 
+/* --- Arrays --- */ 
+
+// Empty array for sorting
+newArray = [];
+
+// User List
+const userList = [
+  "PixelPalooza",
+  "CosmicChaos",
+  "MindMeldMaster",
+  "SnackAttack87",
+  "NeonNinja",
+  "TechTornado",
+  "MysticMuffin",
+  "AquaAce",
+  "SkySurferX",
+  "SparklingSapphire",
+];
+
+/* --- Functions --- */ 
+
+
+
+
+
+
+
+/* --- Event Listeners --- */ 
+
+// Add Three Users
+addThreeUsers.addEventListener("click", function () {
+  prompt("Please input three new users: ");
+});
+
+// Remove User
+removeUsers.addEventListener("click", function () {
+  prompt("Please select which user you would like to remove: ");
+});
+
+// Display all users
+displayUsers.addEventListener("click", function () {
+  window.alert(userList);
+});
+
+// Sort users
+sortUsers.addEventListener("click", function () {
+});

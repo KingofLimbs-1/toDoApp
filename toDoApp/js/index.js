7b9356d46def122b3f41996e78a3495f483d746c
Miles Pieterse <99418553+KingofLimbs-1@users.noreply.github.com>
2023-03-25 18:14:42 +0200

    Continued Working
    Changes Made:
    
    1. Fixed up comments + made them neater (HTML, JS, CSS)
    
    2. Created a "light/dark mode" button + nested it within the heading for easier styling (HTML)
    
    3. Added two new images to img folder for the toggle button.
    
    4. Began building JS logic + creating element references (JS)
    
    5. Styled "toggle button" for dark-mode version + added first part of styling for "taskList" element (CSS)

---
+++ toDoApp/js/index.js
@@ -0,0 +1,16 @@
+/* --- Element References --- */
+
+// Task Input Field
+const taskInput = document.getElementById("taskInput");
+// Add Task Button
+const addTaskBtn = document.getElementById("add-new-task-button");
+// Task List
+const taskList = document.getElementById("taskList");
+
+/* ---/ Tab References /--- */
+
+/* --- Functionality --- */
+/* ---/ Functionality /--- */
+
+/* --- Event Listeners --- */
+/* ---/ Event Listeners /--- */

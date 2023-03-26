0a90d444429991b6518d035f7487699735eed07a
Miles Pieterse <99418553+KingofLimbs-1@users.noreply.github.com>
2023-03-26 23:03:34 +0200

    Continued Working
    Changes Made:
    
    1. Added ID to <body> tag for JS & CSS reference when creating light-mode feature + Edited comments (HTML)
    
    2. Created reference to <body> tag using newly created ID & created a reference to toggle button (JS)
    
    3. Organized CSS for improved readability. (CSS)

--- toDoApp/js/index.js
+++ toDoApp/js/index.js
@@ -1,16 +1,24 @@
 /* --- Element References --- */
 
+// Body
+const allElements = document.getElementById("all-elements");
 // Task Input Field
 const taskInput = document.getElementById("taskInput");
 // Add Task Button
 const addTaskBtn = document.getElementById("add-new-task-button");
 // Task List
 const taskList = document.getElementById("taskList");
+// Toggle Button
+const toggle = document.querySelector(".toggle");
 
-/* ---/ Tab References /--- */
+/* ---/ Element References /--- */
 
 /* --- Functionality --- */
 /* ---/ Functionality /--- */
 
 /* --- Event Listeners --- */
+// Light Mode Switch Event
+toggle.addEventListener("click", function () {
+  allElements.classList.toggle("light-mode");
+});
 /* ---/ Event Listeners /--- */

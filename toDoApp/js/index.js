58f4175ef45eb587c48c1ef66953e4d9d887d612
Miles Pieterse <99418553+KingofLimbs-1@users.noreply.github.com>
2023-03-30 20:29:15 +0200

    Continued Working
    Changes Made:
    
    -- JS --
    
    1. Added display logic to the "incompleteTasks" section by adding onto "addTaskBtn" event listener
    
    2. Added display logic to the "completedTasks" section by adding onto "completeTasks" function
    
    3. Created "edit" and "delete" button elements & added them to the "taskList" list items
    
    4. Created "edit" and "delete" button event listeners
    
    -- CSS --
    
    1. Added display styling to sections
    
    2. Added general styling to "edit" and "delete" buttons

--- toDoApp/js/index.js
+++ toDoApp/js/index.js
@@ -11,9 +11,9 @@ const taskList = document.getElementById("taskList");
 // Toggle Button
 const toggle = document.querySelector(".toggle");
 // Completed Task List
-const completedTasks = document.getElementById("completedTasks");
+const completedTaskList = document.getElementById("completedTaskList");
 // Task Data Storage
-const tasks = [];
+let tasks = [];
 
 /* ---/ Element References /--- */
 
@@ -30,37 +30,62 @@ function addTask() {
     });
     // Incomplete task "li" creation
     const li = document.createElement("li");
+    // "li" content variable assignment
+    li.textContent = taskText;
+
     // Checkbox element creation
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
-    // "li" content variable assignment
-    li.textContent = taskText;
-    // Checkbox append to task list "li"
     li.appendChild(checkbox);
+
+    // Delete button creation
+    const deleteBtn = document.createElement("button");
+    deleteBtn.className = "deleteBtn";
+    deleteBtn.textContent = "Delete";
+    li.appendChild(deleteBtn);
+
+    // Edit button creation
+    const editBtn = document.createElement("button");
+    editBtn.textContent = "Edit";
+    editBtn.className = "editBtn";
+    li.appendChild(editBtn);
+    
     // "li" append to taskList
     taskList.appendChild(li);
     // "taskInput" field clear
     taskInput.value = "";
   }
 }
+
 // "Add completed task to completed tasks list" function
 function completeTask(event) {
+  // "completeTasks" section reference
+  const completeTasks = document.querySelector("section.completeTasks");
+  // Display logic to hide aforementioned section when page is first displayed
+  if (
+    completeTasks.style.display === "none" ||
+    completeTasks.style.display === ""
+  ) {
+    completeTasks.style.display = "block";
+  }
+  
   // Task list checkbox reference
   const checkbox = event.target;
   // List item (checkbox parent) reference
   const li = checkbox.parentNode;
+   // Task content reference
+   const taskContent = li.firstChild.textContent;
   // Completed task "li" creation
   const completedTask = document.createElement("li");
   // Task text node creation
-  const completedText = document.createTextNode(li.textContent);
+  const completedText = document.createTextNode(taskContent);
   // Text node append to completed task "li"
   completedTask.appendChild(completedText);
-  // Completed task append to "completedTasks" list
-  completedTasks.appendChild(completedTask);
+  // Completed task append to "completedTaskList" list
+  completedTaskList.appendChild(completedTask);
   // "li" + task text removal from "taskList"
   taskList.removeChild(li);
 }
-/* 
 /* ---/ Functionality /--- */
 
 /* --- Event Listeners --- */
@@ -83,6 +108,15 @@ toggle.addEventListener("click", function () {
 
 // Add Task Button Event
 addTaskBtn.addEventListener("click", function (event) {
+  // "incompleteTasks" section reference
+  const incompleteTasks = document.querySelector("section.incompleteTasks");
+  // Display logic to hide aforementioned section when page is first displayed
+  if (
+    incompleteTasks.style.display === "none" ||
+    incompleteTasks.style.display === ""
+  ) {
+    incompleteTasks.style.display = "block";
+  }
   // Prevent form default behavior
   event.preventDefault();
   addTask();
@@ -90,4 +124,49 @@ addTaskBtn.addEventListener("click", function (event) {
 
 // Checkbox Event
 taskList.addEventListener("change", completeTask);
+
+// Edit Button Event
+taskList.addEventListener("click", function (event) {
+  const target = event.target;
+
+  // Text edit logic
+  if (target.textContent === "Edit") {
+    // taskList "li" element grab
+    const li = target.parentNode;
+    // Makes "li" content editable
+    li.contentEditable = true;
+    // "editBtn" text change
+    target.textContent = "Save";
+
+    // Save edited text logic
+  } else if (target.textContent === "Save") {
+    // Another taskList "li" element grab
+    const li = target.parentNode;
+    // Makes "li" content un-editable
+    li.contentEditable = false;
+    // Switches back "editBtn" text
+    target.textContent = "Edit";
+    // "taskList" array conversion + "li" element grab
+    const taskIndex = Array.from(taskList.children).indexOf(li);
+    // Task text update with new user input
+    tasks[taskIndex].text = li.textContent.trim();
+  }
+});
+
+// Delete button Event
+taskList.addEventListener("click", function (event) {
+  const target = event.target;
+
+  // taskList item removal logic
+  if (target.textContent === "Delete") {
+    // taskList "li" grab
+    const li = target.parentNode;
+    // taskList array conversion + "li" element grab
+    const taskIndex = Array.from(taskList.children).indexOf(li);
+    // "tasks" array task removal
+    tasks.splice(taskIndex, 1);
+    // Task element removal
+    taskList.removeChild(li);
+  }
+});
 /* ---/ Event Listeners /--- */
